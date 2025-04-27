6ae55a45c49e56a8b2d38924b173617604c0c564
yina <yinaofficeg@gmail.com>
2025-04-27 15:27:18 +0900

    Config: JpaConfig 추가, @EnableJpaAuditing 활성화
    - AuditingEntityListener 사용하기 위함(생성,수정 기록)

---
+++ src/main/java/com/kcd/pos/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.kcd.pos.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing // AuditingEntityListener 사용하기 위함
+public class JpaConfig {
+
+}
