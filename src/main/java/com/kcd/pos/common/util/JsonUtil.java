b711b33c46e23f3e88ff50db52599be34a76a3cf
yina <yinaofficeg@gmail.com>
2025-04-30 13:20:35 +0900

    Add: JsonUtil

---
+++ src/main/java/com/kcd/pos/common/util/JsonUtil.java
@@ -0,0 +1,17 @@
+package com.kcd.pos.common.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonUtil {
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    public static String toJson(Object o){
+        try {
+            return mapper.writeValueAsString(o);
+        } catch (JsonProcessingException e) {
+            return "Json serialize failed: " + o.toString();
+        }
+
+    }
+}
