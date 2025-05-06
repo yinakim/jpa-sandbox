6e7d4240d37f8d1b3fdada6e8f597e580f8fbf38
yina <yinaofficeg@gmail.com>
2025-05-07 01:25:41 +0900

    @RestControllerAdvice 어노테이션 정정

--- src/main/java/com/kcd/pos/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kcd/pos/common/exception/GlobalExceptionHandler.java
@@ -2,10 +2,10 @@ package com.kcd.pos.common.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
